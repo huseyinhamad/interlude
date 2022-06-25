69a0c34b81d9f478f6fb01766754daca48ca5fce
Hüseyin Hamad <huseyinnhamad@gmail.com>
2022-06-25 16:44:44 +0300

    INT-4 : MainLayout of the frontend part is constructed.

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,116 +1,105 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <div id="q-app" style="min-height: 100vh">
+    <div class="q-pa-md">
+      <q-layout view="lHh Lpr lFf">
+        <q-header elevated class="bg-dark">
+          <q-toolbar>
+            <q-btn
+              flat
+              round
+              dense
+              icon="menu"
+              class="q-mr-sm"
+              @click="toggleLeftDrawer"
+            ></q-btn>
+            <q-toolbar-title style="text-align: center">
+              Interlude
+            </q-toolbar-title>
+            <q-btn round dense icon="refresh" class="q-mr-md"></q-btn>
+            <q-btn round dense icon="logout" class="q-mr-sm"></q-btn>
+          </q-toolbar>
+        </q-header>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
+        <q-drawer
+          class="bg-dark text-white"
+          v-model="drawer"
+          show-if-above="false"
+          bordered
+          :width="200"
+          :breakpoint="400"
         >
-          Essential Links
-        </q-item-label>
+          <q-img
+            class="absolute-top"
+            src="https://cdn.quasar.dev/img/material.png"
+            style="height: 150px"
+          >
+            <div class="absolute-bottom bg-transparent">
+              <q-avatar size="56px" class="q-mb-sm">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+              <div class="text-weight-bold">Hüseyin Hamad</div>
+              <div>@huseyinhamad</div>
+            </div>
+          </q-img>
+          <q-scroll-area
+            style="
+              height: calc(100% - 150px);
+              margin-top: 150px;
+              border-right: 1px solid #ddd;
+            "
+          >
+            <q-list class="text-white" padding>
+              <DrawerLinks
+                v-for="link in drawerLinks"
+                :key="link.title"
+                v-bind="link"
+              />
+            </q-list>
+          </q-scroll-area>
+        </q-drawer>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+        <q-page-container>
+          <router-view />
+        </q-page-container>
+      </q-layout>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
+import DrawerLinks from "src/components/DrawerLinks.vue";
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Home",
+    icon: "home",
+    link: "#/home",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Profile",
+    icon: "account_circle",
+    link: "#/profile",
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-    EssentialLink
+    DrawerLinks,
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const drawer = ref(false);
 
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      drawerLinks: linksList,
+      drawer,
+      toggleLeftDrawer() {
+        drawer.value = !drawer.value;
+      },
+    };
+  },
+});
 </script>
